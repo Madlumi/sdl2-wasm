8ee5547897f7b251224ad21137e8cf7078c8dafa
madi <madi@localhost.localdomain>
2025-07-01 16:17:44 +0300

    insq bug fix

--- src/mutilSDL.h
+++ src/mutilSDL.h
@@ -2,7 +2,7 @@
 #include "SDL_rect.h"
 #include "mutil.h"
 #include "SDL_stdinc.h"
-#define INSQ(p, a) ( IN(p.x, a.x, a.x+a.w) && IN(p.y, a.y, a.x+a.h))
+#define INSQ(p, a) ( IN(p.x, a.x, a.x+a.w) && IN(p.y, a.y, a.y+a.h))
 
 typedef Uint32 U32;
 typedef SDL_Rect RECT;
