1897728b34c5a1ab2ee8d08b8e9d36f65ee3718e
Madanie <61489225+Madlumi@users.noreply.github.com>
2025-12-02 02:49:50 +0200

    Tile floor and ceiling meshes

--- src/EGAME/wolf3d.c
+++ src/EGAME/wolf3d.c
@@ -24,7 +24,10 @@ static const float WALL_HEIGHT = 1.5f;
 
 static MeshInstance faces[MAP_W * MAP_H * 6];
 static int faceCount = 0;
-static MeshInstance floorMeshes[2];
+static MeshInstance floorFaces[MAP_W * MAP_H];
+static MeshInstance ceilingFaces[MAP_W * MAP_H];
+static int floorCount = 0;
+static int ceilingCount = 0;
 
 static Vec3 camPos = {1.5f, 0.4f, 1.5f};
 static float camYaw = 0.0f;
@@ -67,28 +70,38 @@ static void buildLevel(void) {
 }
 
 static void buildFloor(void) {
-    Vec3 floorVerts[4] = {
-        v3(0, 0, 0),
-        v3(MAP_W * TILE_SIZE, 0, 0),
-        v3(MAP_W * TILE_SIZE, 0, MAP_H * TILE_SIZE),
-        v3(0, 0, MAP_H * TILE_SIZE)
-    };
-    Vec3 ceilingVerts[4] = {
-        v3(0, WALL_HEIGHT, 0),
-        v3(MAP_W * TILE_SIZE, WALL_HEIGHT, 0),
-        v3(MAP_W * TILE_SIZE, WALL_HEIGHT, MAP_H * TILE_SIZE),
-        v3(0, WALL_HEIGHT, MAP_H * TILE_SIZE)
-    };
+    floorCount = 0;
+    ceilingCount = 0;
 
     SDL_Color floorColor = {70, 90, 110, 255};
     SDL_Color ceilingColor = {40, 45, 65, 255};
     float tileScale = 0.5f;
-    SDL_FPoint floorUV[4] = {{0.0f, 0.0f}, {MAP_W * tileScale, 0.0f}, {MAP_W * tileScale, MAP_H * tileScale}, {0.0f, MAP_H * tileScale}};
-    SDL_FPoint ceilUV[4] = {{0.0f, 0.0f}, {MAP_W * tileScale, 0.0f}, {MAP_W * tileScale, MAP_H * tileScale}, {0.0f, MAP_H * tileScale}};
-    render3dInitQuadMeshUV(&floorMeshes[0], floorVerts[0], floorVerts[1], floorVerts[2], floorVerts[3], floorColor,
-                           floorUV[0], floorUV[1], floorUV[2], floorUV[3]);
-    render3dInitQuadMeshUV(&floorMeshes[1], ceilingVerts[0], ceilingVerts[1], ceilingVerts[2], ceilingVerts[3], ceilingColor,
-                           ceilUV[0], ceilUV[1], ceilUV[2], ceilUV[3]);
+
+    for (int z = 0; z < MAP_H; z++) {
+        for (int x = 0; x < MAP_W; x++) {
+            Vec3 f0 = v3(x * TILE_SIZE, 0, z * TILE_SIZE);
+            Vec3 f1 = v3((x + 1) * TILE_SIZE, 0, z * TILE_SIZE);
+            Vec3 f2 = v3((x + 1) * TILE_SIZE, 0, (z + 1) * TILE_SIZE);
+            Vec3 f3 = v3(x * TILE_SIZE, 0, (z + 1) * TILE_SIZE);
+
+            Vec3 c0 = v3(x * TILE_SIZE, WALL_HEIGHT, z * TILE_SIZE);
+            Vec3 c1 = v3((x + 1) * TILE_SIZE, WALL_HEIGHT, z * TILE_SIZE);
+            Vec3 c2 = v3((x + 1) * TILE_SIZE, WALL_HEIGHT, (z + 1) * TILE_SIZE);
+            Vec3 c3 = v3(x * TILE_SIZE, WALL_HEIGHT, (z + 1) * TILE_SIZE);
+
+            SDL_FPoint uv0 = {0.0f, 0.0f};
+            SDL_FPoint uv1 = {tileScale, 0.0f};
+            SDL_FPoint uv2 = {tileScale, tileScale};
+            SDL_FPoint uv3 = {0.0f, tileScale};
+
+            if (floorCount < (int)(sizeof(floorFaces) / sizeof(floorFaces[0]))) {
+                render3dInitQuadMeshUV(&floorFaces[floorCount++], f0, f1, f2, f3, floorColor, uv0, uv1, uv2, uv3);
+            }
+            if (ceilingCount < (int)(sizeof(ceilingFaces) / sizeof(ceilingFaces[0]))) {
+                render3dInitQuadMeshUV(&ceilingFaces[ceilingCount++], c0, c1, c2, c3, ceilingColor, uv0, uv1, uv2, uv3);
+            }
+        }
+    }
 }
 
 void wolf3dInit() {
@@ -132,8 +145,12 @@ void wolf3dRender(SDL_Renderer *renderer) {
     Camera3D cam = {camPos, camYaw, camPitch, fov};
     render3dSetCamera(cam);
 
-    drawMesh(renderer, &floorMeshes[0].mesh, floorMeshes[0].position, floorMeshes[0].rotation, floorMeshes[0].color);
-    drawMesh(renderer, &floorMeshes[1].mesh, floorMeshes[1].position, floorMeshes[1].rotation, floorMeshes[1].color);
+    for (int i = 0; i < floorCount; i++) {
+        drawMesh(renderer, &floorFaces[i].mesh, floorFaces[i].position, floorFaces[i].rotation, floorFaces[i].color);
+    }
+    for (int i = 0; i < ceilingCount; i++) {
+        drawMesh(renderer, &ceilingFaces[i].mesh, ceilingFaces[i].position, ceilingFaces[i].rotation, ceilingFaces[i].color);
+    }
 
     FaceDepth order[MAP_W * MAP_H * 6];
     for (int i = 0; i < faceCount; i++) {
